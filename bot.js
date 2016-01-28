749a68c007c9b3135c6bcf55f56dfaabd530482b
potatocone <henderson@hendersonnguyen.com>
2016-01-28 13:30:29 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,39 +14,39 @@ function respond() {
   } 
   else if(request.text && botRegexTits.test(request.text)) {
     this.res.writeHead(200);
-    if(Math.random() > 0.4)
+    if(Math.random() > 0.4) {
       postMessage("http://i.imgur.com/F2vcuNf.jpg");
-    else 
+    } else {
       postMessage("http://i.imgur.com/0RE6wLK.jpg"); 
-    this.res.end();
+    } this.res.end();
   }
   else if(request.text && botRegexBoobs.test(request.text)) {
     this.res.writeHead(200);
-    if(Math.random() > 0.4)
+    if(Math.random() > 0.4) {
       postMessage("http://i.imgur.com/znjGVxG.jpg");
-    else
+    } else {
       postMessage("http://i.imgur.com/zA5taR3.jpg");
-    this.res.end();
+    } this.res.end();
   }  
   else if(request.text && botRegexBoobies.test(request.text)) {
     this.res.writeHead(200);
-    if(Math.random() > 0.4)
+    if(Math.random() > 0.4) {
       postmessage("http://i.imgur.com/G5DkpRH.jpg");
-    else
+    } else {
       postmessage("http://i.imgur.com/wnunRus.jpg");
-    this.res.end();
+    } this.res.end();
   }
   else if(request.text && botRegexcheese.test(request.text)) {
     this.res.writeHead(200);
-    if(0.75 >= Math.random() > 0.5)
+    if(0.75 >= Math.random() > 0.5) {
       postMessage("https://i.groupme.com/500x477.png.51b13f3706b9414c85ad831ae4352361");
-    else if(0.5 >= Math.random() > 0.25)
+    } else if(0.5 >= Math.random() > 0.25) {
       postMessage("https://i.groupme.com/400x400.gif.88ed0db90e6a4bfd937588d3ac09e66d");
-    else if(Math.random() >0.75)
+    } else if(Math.random() >0.75) {
       postMessage("http://i.imgur.com/MQmtrNp.jpg")
-    else
+    } else {
       postMessage("http://cdn.makeagif.com/media/1-24-2014/zaAiBl.gif");
-    this.res.end();
+    } this.res.end();
   }
   else {
     console.log("don't care");
